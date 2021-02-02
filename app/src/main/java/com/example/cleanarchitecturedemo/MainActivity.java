4c6b270f5900d48989c22869e19d1c68cfcf1fc8
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2021-02-02 16:53:51 +0100

    Add setOnClickListener. Write in editText, click button and text appears in outputView.

--- app/src/main/java/com/example/cleanarchitecturedemo/MainActivity.java
+++ app/src/main/java/com/example/cleanarchitecturedemo/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.cleanarchitecturedemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+EditText editText;
+TextView outputView;
+Button enterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.editText);
+        outputView = (TextView)findViewById(R.id.outputView);
+        enterButton = (Button)findViewById(R.id.enterButton);
+
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            String string = editText.getText().toString();
+            outputView.setText(string);
+            }
+        });
     }
 }
