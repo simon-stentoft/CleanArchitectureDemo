c599f3a663f7ba8e6afde2dea410675036641b5a
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2021-02-04 16:44:35 +0100

    editText og outputView bruger nu model for at få data. Har skabt getter og setter for string data, og sat data = "Hej".

--- app/src/main/java/model/Model.java
+++ app/src/main/java/model/Model.java
@@ -1,5 +1,14 @@
 package model;
 
 public class Model {
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    private String data = "Hej";
 
 }
--- app/src/main/java/view/AndroidView.java
+++ app/src/main/java/view/AndroidView.java
@@ -10,18 +10,27 @@ import android.widget.TextView;
 
 import com.example.cleanarchitecturedemo.R;
 
+import model.Model;
+
 public class AndroidView extends AppCompatActivity {
 EditText editText;
 TextView outputView;
 Button enterButton;
+
+private Model model = new Model();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         editText = (EditText)findViewById(R.id.editText);
+        editText.setText(model.getData()); //Anvender nu model for at få data
+
         outputView = (TextView)findViewById(R.id.outputView);
+        outputView.setText(model.getData()); //Anvender nu model for at få data
+
         enterButton = (Button)findViewById(R.id.enterButton);
 
         enterButton.setOnClickListener(new View.OnClickListener() {
@@ -31,5 +40,6 @@ Button enterButton;
             outputView.setText(string);
             }
         });
+
     }
 }
